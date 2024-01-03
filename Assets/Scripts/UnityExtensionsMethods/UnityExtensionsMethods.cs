69e396311407cefc31bd9a7af207b35b2285a1d8
quentin.chauvelon@grenoble-inp.org <>
2024-01-03 11:55:48 +0100

    Added DateTime / Unix timestamp conversion extensions methods

---
+++ Assets/Scripts/UnityExtensionsMethods/UnityExtensionsMethods.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace UnityExtensionMethods
+{
+    public static class UnityExtensionsMethods
+    {
+        /// <summary>
+        /// Converts a DateTime object to a Unix timestamp.
+        /// </summary>
+        /// <param name="date">The date to convert to a Unix timestamp</param>
+        /// <returns>The number of seconds that have passed since the epoch</returns>
+        public static int DateToUnixTimestamp(this DateTime date)
+        {
+            return (int)Math.Floor((date - DateTime.UnixEpoch).TotalSeconds);
+        }
+
+
+        /// <summary>
+        /// Converts a Unix timestamp to a DateTime object.
+        /// </summary>
+        /// <param name="timestamp">The Unix timestamp to convert to a date</param>
+        /// <returns></returns>
+        public static DateTime UnixTimestampToDate(this int timestamp)
+        {
+            return DateTime.UnixEpoch.AddSeconds(timestamp);
+        }
+
+
+        /// <summary>
+        /// Checks if two floats are equal within a certain epsilon.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="epsilon"></param>
+        /// <returns></returns>
+        public static bool FloatEquals(this float a, float b, float epsilon = 0.0001f)
+        {
+            return Math.Abs(a - b) < epsilon;
+        }
+
+        
+        /// <summary>
+        /// Checks if two doubles are equal within a certain epsilon.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="epsilon"></param>
+        /// <returns></returns>
+        public static bool DoubleEquals(this double a, double b, double epsilon = 0.0001)
+        {
+            return Math.Abs(a - b) < epsilon;
+        }
+    }
+}
