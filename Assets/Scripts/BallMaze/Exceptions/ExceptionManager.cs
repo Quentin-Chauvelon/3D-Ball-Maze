f9308fe1123c5bd4ac3de1815d396a1ee5f2f84d
quentin.chauvelon@grenoble-inp.org <>
2024-01-03 11:53:16 +0100

    Added ExceptionManager to display errors to the player

---
+++ Assets/Scripts/BallMaze/Exceptions/ExceptionManager.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+
+
+namespace BallMaze
+{
+    public class ExceptionManager : MonoBehaviour
+    {
+        // Singleton pattern
+        private static ExceptionManager _instance;
+        public static ExceptionManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    Debug.LogError("ExceptionManager is null!");
+                }
+                return _instance;
+            }
+        }
+
+
+        private void Awake()
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+
+        /// <summary>
+        /// Displays the given exception message
+        /// </summary>
+        /// <param name="message">The message to display</param>
+        /// <param name="action">Defines what buttons to display to the user (by default go back to the main menu)</param>
+        public void ShowExceptionMessage(string message, ExceptionAction action = ExceptionAction.BackToMainMenu)
+        {
+            Debug.Log(message);
+            // TODO: UI to display message
+            // TODO: add buttons according to the given action
+        }
+
+
+        /// <summary>
+        /// Possible actions to display to the player when an exception occurs
+        /// </summary>
+        public enum ExceptionAction
+        {
+            BackToMainMenu,
+            BackToLevels
+        }
+    }
+}
