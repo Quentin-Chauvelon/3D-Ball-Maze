541ad4c8c88ace07bde6165e6bcbb72726a4b302
Quentin-Chauvelon <quentin.chauvelon@gmail.com>
2024-01-13 17:40:03 +0100

    Added ball base logic

--- Assets/Scripts/BallMaze/Game/Ball.cs
+++ Assets/Scripts/BallMaze/Game/Ball.cs
@@ -1,29 +1,73 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+
+namespace BallMaze
 {
-    private Rigidbody _rigidbody;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Ball : MonoBehaviour
     {
-        _rigidbody = GetComponent<Rigidbody>();
-    }
+        private Rigidbody _rigidbody;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        // Change this value to play with the ball's speed
+        private const short FORCE_SPEED_DIVIDER = 100;
+
+        void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
 
 
-    /// <summary>
-    /// Freeze or unfreeze the ball.
-    /// </summary>
-    internal void FreezeBall(bool freeze)
-    {
-        _rigidbody.isKinematic = freeze;
+        /// <summary>
+        /// Freeze or unfreeze the ball.
+        /// </summary>
+        public void FreezeBall(bool freeze)
+        {
+            _rigidbody.isKinematic = freeze;
+        }
+
+
+        /// <summary>
+        /// Set the ball's visibility.
+        /// </summary>
+        /// <param name="visible"></param>
+        public void SetBallVisible(bool visible)
+        {
+            gameObject.SetActive(visible);
+        }
+
+
+        /// <summary>
+        /// Moves the ball to the given position.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="resetRotation">Boolean indicating if the ball should keep its rotation</param>
+        public void MoveBallToPosition(Vector3 position, bool resetRotation = true)
+        {
+            gameObject.transform.localPosition = position;
+
+            if (resetRotation)
+            {
+                gameObject.transform.rotation = Quaternion.identity;
+            }
+        }
+
+
+        /// <summary>
+        /// Adds a force to the ball to make it move faster through the maze. Makes it more enjoyable to play.
+        /// Also helps changing direction quicker.
+        /// </summary>
+        /// <param name="direction">The direction to move the ball towards. Usually the same direction as the joystick or accelerometer</param>
+        public void AddForce(Vector2 direction)
+        {
+            _rigidbody.AddForce(new Vector3(direction.x, 0f, direction.y) / FORCE_SPEED_DIVIDER, ForceMode.Impulse);
+        }
     }
 }
