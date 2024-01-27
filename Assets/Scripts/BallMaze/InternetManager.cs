aeaa45010afbbe40401ef8c74b4f534d44b1acd8
Quentin-Chauvelon <quentin.chauvelon@gmail.com>
2024-01-27 22:46:45 +0100

    Refactored InternetManager ping methods.
    - Changed Ping() method from a coroutine to an async method
    - Removed CheckPing() as it wasn't needed anymore because we can simply await the Task<bool> returned from Ping()
    - Renamed isCheckOnlineLoopRunning to _isCheckOnlineLoopRunning for consistency with the naming style in the rest of the codebase

--- Assets/Scripts/BallMaze/InternetManager.cs
+++ Assets/Scripts/BallMaze/InternetManager.cs
@@ -4,12 +4,13 @@ using System.Threading.Tasks;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Networking;
+using Redcode.Awaiting;
 
 
 namespace BallMaze
 {
-public class InternetManager : MonoBehaviour
-{
+    public class InternetManager : MonoBehaviour
+    {
         // Singleton pattern
         private static InternetManager _instance;
         public static InternetManager Instance
@@ -24,150 +25,140 @@ public class InternetManager : MonoBehaviour
             }
         }
 
-    private bool _wasOnline;
-    private bool _isOnline;
-    public bool isOnline
-    {
-        get { return _isOnline; }
-        set
+        private bool _wasOnline;
+        private bool _isOnline;
+        public bool isOnline
         {
-            _wasOnline = _isOnline;
-            _isOnline = value;
-
-            // If the internet was not available and now it is, start the loop to check every few seconds if it's still available
-            if (_isOnline && !isCheckOnlineLoopRunning)
+            get { return _isOnline; }
+            set
             {
-                CheckIsOnlineLoop();
+                _wasOnline = _isOnline;
+                _isOnline = value;
+
+                // If the internet was not available and now it is, start the loop to check every few seconds if it's still available
+                if (_isOnline && !_isCheckOnlineLoopRunning)
+                {
+                    CheckIsOnlineLoop();
+                }
             }
         }
-    }
 
-    // Variable to know if the first check has completed
-    public bool initialized;
+        // Variable to know if the first check has completed
+        public bool initialized;
 
-    private bool isCheckOnlineLoopRunning;
+        private bool _isCheckOnlineLoopRunning;
 
-    private const string PING_ADDRESS = "www.google.com";
-    // If internet is available, check every 30 seconds if it's still available
-    private const int CHECK_IS_ONLINE_DELAY = 30;
-    // Maximum number of attempts to check if the internet is available (only on Initialize());
-    private const int MAX_ATTEMPTS = 5;
+        private const string PING_ADDRESS = "www.google.com";
+        // If internet is available, check every 30 seconds if it's still available
+        private const int CHECK_IS_ONLINE_DELAY = 30;
+        // Maximum number of attempts to check if the internet is available (only on Initialize());
+        private const int MAX_ATTEMPTS = 5;
 
 
-    /// <summary>
-    /// On start, check if the internet is available.
-    /// If it's not, try again every 5 seconds until it is for up to 5 times.
-    /// </summary>
+        /// <summary>
+        /// On start, check if the internet is available.
+        /// If it's not, try again every 5 seconds until it is for up to 5 times.
+        /// </summary>
         private async void Awake()
-    {
+        {
             _instance = this;
             DontDestroyOnLoad(this);
 
-        _wasOnline = false;
-        isOnline = false;
-        initialized = false;
+            _wasOnline = false;
+            isOnline = false;
+            initialized = false;
 
-        // Retry up to 5 times to check if the internet is available
-        for (int i = 0; i < MAX_ATTEMPTS; i++)
-        {
-            bool result = await CheckIsOnline();
-            if (result)
+            // Retry up to 5 times to check if the internet is available
+            for (int i = 0; i < MAX_ATTEMPTS; i++)
             {
-                break;
+                bool result = await CheckIsOnline();
+                if (result)
+                {
+                    break;
+                }
+                else
+                {
+                    await Task.Delay(5000);
+                }
+            }
+
+            // Set initialized to true so that other script can know that if isOnline is false,
+            // it's because internet is not available and not because the ping is still running
+            initialized = true;
+            InternetManagerEvents.intialized?.Invoke();
+        }
+
+
+        /// <summary>
+        /// Check every few seconds if the internet is still available.
+        /// This allows to detect if the player loses the internet connection while playing.
+        /// </summary>
+        public async void CheckIsOnlineLoop()
+        {
+            if (_isCheckOnlineLoopRunning)
+            {
+                return;
+            }
+
+            _isCheckOnlineLoopRunning = true;
+
+            while (true)
+            {
+                await Task.Delay(CHECK_IS_ONLINE_DELAY * 1000);
+
+                bool result = await CheckIsOnline();
+                if (!result)
+                {
+                    break;
+                }
+            }
+
+            _isCheckOnlineLoopRunning = false;
+        }
+
+
+        /// <summary>
+        /// Check if the internet is available by pinging the given address.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> CheckIsOnline()
+        {
+            // If the no network is available, we can't be connected to the internet
+            if (Application.internetReachability == NetworkReachability.NotReachable)
+            {
+                isOnline = false;
+                return false;
+            }
+
+            await Ping();
+
+            return isOnline;
+        }
+
+
+
+
+
+        /// <summary>
+        /// Ping the given address to check if the internet is available.
+        /// </summary>
+        /// <returns></returns>
+        private async Task<bool> Ping()
+        {
+            UnityWebRequest request = new UnityWebRequest(PING_ADDRESS);
+            request.timeout = 3;
+            await request.SendWebRequest();
+
+            if (request.responseCode < 299 && string.IsNullOrEmpty(request.error))
+            {
+                isOnline = true;
+                return true;
             }
             else
             {
-                await Task.Delay(5000);
+                isOnline = false;
+                return false;
             }
         }
-
-        // Set initialized to true so that other script can know that if isOnline is false,
-        // it's because internet is not available and not because the ping is still running
-        initialized = true;
     }
-
-
-    /// <summary>
-    /// Check every few seconds if the internet is still available.
-    /// This allows to detect if the player loses the internet connection while playing.
-    /// </summary>
-    public async void CheckIsOnlineLoop()
-    {
-        if (isCheckOnlineLoopRunning)
-        {
-            return;
-        }
-
-        isCheckOnlineLoopRunning = true;
-
-        while (true)
-        {
-            await Task.Delay(CHECK_IS_ONLINE_DELAY * 1000);
-
-            bool result = await CheckIsOnline();
-            if (!result)
-            {
-                break;
-            }
-        }
-
-        isCheckOnlineLoopRunning = false;
-    }
-
-
-    /// <summary>
-    /// Check if the internet is available by pinging the given address.
-    /// </summary>
-    /// <returns></returns>
-    public async Task<bool> CheckIsOnline()
-    {
-        // If the no network is available, we can't be connected to the internet
-        if (Application.internetReachability == NetworkReachability.NotReachable)
-        {
-            isOnline = false;
-            return false;
-        }
-
-        await CheckPing();
-
-        return isOnline;
-    }
-
-
-    /// <summary>
-    /// Starts a coroutine to ping the given address to check if the internet is available.
-    /// </summary>
-    /// <returns></returns>
-    private async Task<bool> CheckPing()
-    {
-        var task = new TaskCompletionSource<bool>();
-        StartCoroutine(Ping(task));
-        await task.Task;
-
-        return task.Task.Result;
-    }
-
-
-    /// <summary>
-    /// Ping the given address to check if the internet is available.
-    /// </summary>
-    /// <param name="task"></param>
-    /// <returns></returns>
-    private IEnumerator Ping(TaskCompletionSource<bool> task)
-    {
-        UnityWebRequest request = new UnityWebRequest(PING_ADDRESS);
-        yield return request.SendWebRequest();
-
-        if (request.responseCode < 299 && string.IsNullOrEmpty(request.error))
-        {
-            isOnline = true;
-            task.SetResult(true);
-        }
-        else
-        {
-            isOnline = false;
-            task.SetResult(false);
-        }
-    }
-}
 }
