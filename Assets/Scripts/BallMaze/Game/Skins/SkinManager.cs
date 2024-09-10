91eb0632d4d76583e112e5de174bda9fb79c8200
Quentin-Chauvelon <quentin.chauvelon@gmail.com>
2024-09-10 18:11:43 +0200

    Add skins list loading from JSON file
    - Add JSON skins list adressable
    - Add skins list adressable loading

--- Assets/Scripts/BallMaze/Game/Skins/SkinManager.cs
+++ Assets/Scripts/BallMaze/Game/Skins/SkinManager.cs
@@ -1,6 +1,10 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 
 namespace BallMaze
@@ -10,12 +14,60 @@ namespace BallMaze
         Common,
         Rare,
         Epic,
-        Legendary
+        Unique,
+        Flags,
+        Unknown
     }
 
 
-    public class SkinManager
+    public class Skin
     {
+        public int id { get; set; }
+        public string name { get; set; }
+        public SkinRarity category { get; set; }
+        public int price { get; set; }
+        public string materialPath { get; set; }
+        public string meshPath { get; set; }
+        public bool isBuyable { get; set; }
+        public string lockText { get; set; }
+    }
+
+
+    public class SkinManager : MonoBehaviour
+    {
+        // Singleton pattern
+        private static SkinManager _instance;
+        public static SkinManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    Debug.LogError("SkinManager is null!");
+                }
+                return _instance;
+            }
+        }
+
+        // A list of all the skins and their properties (name, rarity, price, materials...)
+        private Skin[] _skinsList;
+
+        private AsyncOperationHandle<TextAsset> _skinsListLoadHandle;
+
+        public static bool IsSkinListLoaded = false;
+        public static Exception SkinListLoadingException = null;
+
+
+        private void Awake()
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            _skinsListLoadHandle = Addressables.LoadAssetAsync<TextAsset>("SkinsList");
+            _skinsListLoadHandle.Completed += OnSkinsListLoaded;
+        }
+
+
         public static string GetSkinRarityName(SkinRarity rarity)
         {
             switch (rarity)
@@ -26,11 +78,42 @@ namespace BallMaze
                     return "Rare";
                 case SkinRarity.Epic:
                     return "Epic";
-                case SkinRarity.Legendary:
-                    return "Legendary";
+                case SkinRarity.Unique:
+                    return "Unique";
+                case SkinRarity.Flags:
+                    return "Flags";
                 default:
                     return "???";
             }
+        }
+
+
+        /// <summary>
+        /// Called asynchronously when the skins list adressable is loaded
+        /// Deserialize the JSON file and save the skins list
+        /// </summary>
+        /// <param name="operation"></param>
+        private void OnSkinsListLoaded(AsyncOperationHandle<TextAsset> operation)
+        {
+            if (operation.Status == AsyncOperationStatus.Succeeded)
+            {
+                _skinsList = JsonConvert.DeserializeObject<Skin[]>(operation.Result.text);
+
+                IsSkinListLoaded = true;
+            }
+            else
+            {
+                // Save the exception that occurred while loading the skins list
+                SkinListLoadingException = operation.OperationException;
+            }
+
+            _skinsListLoadHandle.Completed -= OnSkinsListLoaded;
+        }
+
+
+        private void OnDestroy()
+        {
+            Addressables.Release(_skinsListLoadHandle);
         }
     }
 }
